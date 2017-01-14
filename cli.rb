0242fd5cf23a5ea5411dc584352fce43ad0d343c
Marko Lovic <markolovic33@gmail.com>
2017-01-14 20:03:36 +0100

    WIP Load ActiveRecord asynchronously

--- cli.rb
+++ cli.rb
@@ -5,23 +5,41 @@ def log_time(str)
   puts (Time.new - START_TIME).to_s + '  ' + str
 end
 
-require 'active_record'
-  log_time 'AR loaded'
 require 'thor'
-  log_time 'Thor loaded'
-require 'pp'
-
-require 'lib/thought'
-require 'lib/parser'
+log_time 'Thor loaded'
 require 'lib/editor_input'
-require 'lib/dump'
+
+# TODO problems loading this file without AR
 
 ROOT = File.dirname(__FILE__)
 DB_PATH = File.join(ROOT, 'db/dumper.sqlite3')
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: DB_PATH)
+  require 'active_record'
+  log_time 'AR loaded'
+  require 'pp'
 
-log_time 'AR established connection'
+  require 'lib/thought'
+  log_time 'Thought loaded'
+  require 'lib/parser'
+  require 'lib/dump'
+
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: DB_PATH)
+
+  log_time 'AR established connection'
+
+def init
+  # TODO 
+  #require 'active_record'
+  #log_time 'AR loaded'
+  #require 'pp'
+
+  #require 'lib/parser'
+  #require 'lib/dump'
+
+  #ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: DB_PATH)
+
+  #log_time 'AR established connection'
+end
 
 class Dumper < Thor
   log_time 'Inherited from Thor'
@@ -38,9 +56,12 @@ class Dumper < Thor
     #exit
     unless str # change to ||= syntax
       editor = EditorInput.new
+      # TODO problem with stout going to vim
       editor.start
+      init
       str = editor.get_text
     end
+    init # TODO only if necessary
     dump = Dump.new(str)
     dump.process
   end
@@ -50,6 +71,8 @@ class Dumper < Thor
     thought = Thought.last
 
     editor_in = EditorInput.new(thought.to_dumpfile) # figure this out. use #to_s?
+    editor_in.start
+    init
     str = editor_in.get_text 
 
     return if str == thought.to_dumpfile
@@ -66,12 +89,15 @@ class Dumper < Thor
 
   desc "list", "list all thoughts. Default is only from today."
   method_option :all, aliases: '-a'
+  # TODO -a shouldn't take argument
   def list(tag = nil)
     # optimize later. Array of scopes as symbols? as lambdas?
+    init
     thoughts = options[:all] ? Thought.unscoped : Thought.created_today 
     if tag
       #clean this up
       thoughts = Thought.joins(:tags).where(tags: {name: tag}).merge(thoughts)
+      # does merge do intersection?
     end
     thoughts.each { |th| puts "\n" + th.created_at.strftime('%H:%M') + ' -  ' + th.to_s }
   end
