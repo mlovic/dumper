ea6c165f8454d830a78be2c09bc5332f7f5a1dc3
Marko Lovic <markolovic33@gmail.com>
2017-12-16 17:29:25 +0100

    Change scripts/add_files.rb to suit latest use case

--- scripts/add_files.rb
+++ scripts/add_files.rb
@@ -1,26 +1,25 @@
-require_relative 'lib/dump'
-def lines(f)
-  ls = []
-  f.each_line { |l| ls << l }
-  ls
-end
+#require_relative 'lib/dump'
+$start = Time.now
+
+require 'pp'
+
+require 'lib/dump'
 
 @thoughts = []
-Dir.glob('/home/marko/Downloads/dump/dump/*') do |file_path|
-  file = File.new file_path
-  lines = lines(file)
-  file.close
-  ts = Time.at(lines.pop.strip.to_i)
-  body = lines.join.strip
+Dir.glob('/home/marko/*_dump') do |file_path|
+  file = File.new(file_path)
   file_name = file_path.split('/').last.strip
-  @thoughts << {body:  body,
-                ts:    ts,
+  @thoughts << {body:  file.read.strip,
+                ts:    file.ctime,
                 title: file_name}
 end
 
 @thoughts.sort_by! { |t| t[:ts] }
 
 @thoughts.each do |th|
+  #pp th
+  #require 'irb'
+  #binding.irb
   dump = Dump.new(th[:body], timestamp: th[:ts], title: th[:title])
   dump.process
 end
